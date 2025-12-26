54ebf47f6b767e64794eb28032c70cc9485623e6
wein1m <weynn.sz@gmail.com>
2025-12-26 15:43:29 +0800

    feat: add some helper functions & define macros

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
 #include "raylib.h"
+#include <math.h>
 #include <stdlib.h>
+
+#define BOARD_SIZE 8
+#define TILE_SIZE 42
+#define TILE_TYPES 5
+
+const char tile_chars[TILE_TYPES] = {'#', '@', '$', '%', '&'};
+
+char board[BOARD_SIZE][BOARD_SIZE];
+
+char get_randomTile() { return tile_chars[rand() % TILE_TYPES]; }
+
+void init_board() {
+  for (int y = 0; y < BOARD_SIZE; y++) {
+    for (int x = 0; x < BOARD_SIZE; x++) {
+      board[y][x] = get_randomTile();
+    }
+  }
+}
 
 int main() { return 0; }
